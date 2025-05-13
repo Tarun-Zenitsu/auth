1aaa6c4204136d549eb53055319c0de9b67423bc
Tarunakanta kumbhar <btkkumbhar1998@gmail.com>
2025-05-13 23:11:55 +0530

    ts done

--- app/api/technical/assigned-applications/route.ts
+++ app/api/technical/assigned-applications/route.ts
@@ -82,7 +82,7 @@ import { NextResponse } from "next/server";
 import { auth } from "@/auth";
 import prisma from "@/lib/db";
 
-export async function GET(req: Request) {
+export async function GET() {
   const session = await auth();
 
   if (!session) {
